94f892a35d81ae8a9a654df9a86039c8a93bebdb
lukehancharik <71534507+lukehancharik@users.noreply.github.com>
2020-09-18 21:24:43 -0400

    Add files via upload

---
+++ RecordState.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+while(1){
+    uLCD_main.print("INPUT NOTE USING DIAL");
+    vector<double> freqVector;
+    if (STORE()){
+        freqVector.pushback(readFrequencyInput());
+    }
+}
