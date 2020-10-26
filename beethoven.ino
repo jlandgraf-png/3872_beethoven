135bf9997a9fc18c29d75fad70583a61d76bfec1
jlandgraf-png <57333754+jlandgraf-png@users.noreply.github.com>
2020-10-26 17:49:06 -0400

    Debounces, pullups, playback mode
    - Added debouncing for store button
    - Made inputs use internal pullups (complement the input when read to keep same logic)
    - Tested and Debugged playback mode, play live mode
    - Recording saves correct values once per press
    - Play live working for servos and dial (speaker not tested)
    - Play recording working for servos and dial (speaker not tested)

--- beethoven.ino
+++ beethoven.ino
@@ -1,22 +1,39 @@
 #include <Vector.h>
+#include <Array.h>
 #include <LiquidCrystal.h>
 #include <Servo.h>
 
 
 //-----------------------------------------------------------------------
-// Variable Declarations
 
+// Notes played
+// 1 - A2  - 110.000 Hz
+// 2 - B2  - 123.471 Hz
+// 3 - C3  - 130.813 Hz
+// 4 - D3  - 146.832 Hz
+// 5 - E3  - 164.814 Hz
+// 6 - F3  - 174.614 Hz
+// 7 - G3  - 195.998 Hz
+// 8 - G#3 - 207.652 Hz
+
+
+
+
+// Variable Declarations
 
 // Tie the right pins to the LCD
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-const int resetPin = 6;
-const int stopPin = 7;
+
+// Declaring pins for buttons, frequency dial, and speaker
+const int resetPin = 7;
+const int stopPin = A2;
 const int recordPin = 8;
 const int storePin = 13;
 const int playLivePin = 18;
 const int playRecordingPin = 19;
 const int freqPin = A3;
+const int speakerPin = 6;
 
 // Values associated with the pushbuttons
 bool RESET = false;
@@ -26,9 +43,15 @@ bool STORE = false;
 bool PLAY_LIVE = false;
 bool PLAY_RECORDING = false;
 
+// To begin setting up servos
+Servo servo1, servo2;
+const int min = 480;
+const int max = 2500;
+const int servo1pin = 9;
+const int servo2pin = 10;
 
-Servo servo1, servo2; // These get attached to pins 9 and 10 respectively in setup later
 
+// Values used in the frequency dial
 float freq = 0.0; //associated with frequency pin
 int freqi = 0; // interger associated with frequency
 int prevFreqi = 0; // interger associated with previous frequency (for displaying on LCD)
@@ -36,13 +59,15 @@ int prevFreqi = 0; // interger associated with previous frequency (for displayin
 // State enumeration.
 enum states{idle, stop0, record, play_live, play_recording};
 
-// Variable Declarations
+// Misc variable Declarations
 float resetTimer = 0;
 int state = idle;
 int prevState = idle;
-Vector<int> freqVector;
+
+const int totalVectorSize = 3;
+int freqVector[totalVectorSize];
 int vectorPosition = 0;
-int currentFrequency = 0;
+int vectorSize = 0;
 int storeButtonHeldDown = 0;
 int startedPlayingRecording = 0;
 
@@ -65,57 +90,126 @@ float speaker_out = 0;
 
 void moveMotors(int frequency) {
   // Add switch statement for frequency
-  servo1.write(90);
-  servo2.write(180);
+  switch (frequency) {
+    case 0:
+      servo1.write(0);
+      servo2.write(0);
+      break;
+    case 1:
+      servo1.write(180);
+      servo2.write(180);
+      break;
+    case 2: 
+      servo1.write(135);
+      servo2.write(180);
+      break;
+    case 3:
+      servo1.write(85);
+      servo2.write(180);
+      break;
+    case 4:
+      servo1.write(40);
+      servo2.write(180);
+      break;
+    case 5:
+      servo1.write(180);
+      servo2.write(135);
+      break;
+    case 6:
+      servo1.write(180);
+      servo2.write(85);
+      break;
+    case 7:
+      servo1.write(180);
+      servo2.write(40);
+      break;
+    case 8:
+      servo1.write(130);
+      servo2.write(130);
+      break;
+  }
   return;
 }
 
 void playFrequency(int frequency) {
-  // Add functionality to play tones
+  
+  switch (frequency) {
+    case 0:
+      noTone(speakerPin);
+      break;
+    case 1:
+      tone(speakerPin, 110);
+      break;
+    case 2: 
+      tone(speakerPin, 123.471);
+      break;
+    case 3:
+      tone(speakerPin, 130.813);
+      break;
+    case 4:
+      tone(speakerPin, 146.832);
+      break;
+    case 5:
+      tone(speakerPin, 164.814);
+      break;
+    case 6:
+      tone(speakerPin, 174.614);
+      break;
+    case 7:
+      tone(speakerPin, 195.998);
+      break;
+    case 8:
+      tone(speakerPin, 207.652);
+      break;
+  }
   return;
 }
 
 void updateLCD() {
 
-  // Update state if it has changed
-  if (prevState != state) {
-    prevState = state;
-
     // Clear the top row
-    lcd.setCursor(0, 0);
-    lcd.print("                ");
+    //lcd.setCursor(0, 0);
+    //lcd.print("                ");
 
     // Print the current state
     lcd.setCursor(0, 0);
+    
     switch (state) {
       case idle:
-        lcd.print("IDLE");
+        lcd.print("IDLE            ");
         break;
       case stop0:
-        lcd.print("STOPPED");
+        lcd.print("STOPPED         ");
         break;
       case record:
-        lcd.print("RECORDING");
+        lcd.print("RECORDING       ");
         break;
       case play_live:
-        lcd.print("PLAYING LIVE");
+        lcd.print("PLAYING LIVE    ");
         break;
       case play_recording:
         //lcd.print("PLAYING RECORDING");
-        lcd.print("PLAYBACK MODE");
+        lcd.print("PLAYBACK MODE   ");
         break;
     }
-  }
-  
+    /*
+    lcd.print(freqVector.size());
+    lcd.setCursor(2, 0);
+    for (int i = 0; i < freqVector.size() && i < 16; ++i) {
+      lcd.print("-");
+      lcd.print(freqVector[i]);
+      
+    }
+    lcd.setCursor(0,1);
+    lcd.print(storeButtonHeldDown);
+    */
+  /*
   // Update the frequency if it has changed
-  if (freqi != prevFreqi) {
+  //if (freqi != prevFreqi) {
     prevFreqi = freqi;
-    lcd.setCursor(0, 1);
-    lcd.print("                ");
-    lcd.setCursor(0, 1);
-    lcd.print("Frequency: ");
+    lcd.setCursor(5, 1);
     lcd.print(freqi);
-  }
+  //}*/
   return;
 }
 
@@ -130,18 +224,31 @@ void setup() {
   lcd.begin(16, 2);
 
   // Attach servos (pins 9 and 10)
-  servo1.attach(9);
-  servo2.attach(10);
+  servo1.attach(servo1pin, min+150, max-120);
+  servo2.attach(servo2pin, min, max);
   servo1.write(0);
   servo2.write(0);
 
-  pinMode(resetPin, INPUT);
-  pinMode(stopPin, INPUT);
-  pinMode(recordPin, INPUT);
-  pinMode(storePin, INPUT);
-  pinMode(playLivePin, INPUT);
-  pinMode(playRecordingPin, INPUT);
+  pinMode(resetPin, INPUT_PULLUP);
+  pinMode(stopPin, INPUT_PULLUP);
+  pinMode(recordPin, INPUT_PULLUP);
+  pinMode(storePin, INPUT_PULLUP);
+  pinMode(playLivePin, INPUT_PULLUP);
+  pinMode(playRecordingPin, INPUT_PULLUP);
   pinMode(freqPin, INPUT);
+
+  lcd.setCursor(0,0);
+  lcd.print("IDLE");
+  lcd.setCursor(0,1);
+  lcd.print("Dial:");
+  lcd.setCursor(6,1);
+  lcd.print(", Stored:");
+
+
+  freqVector[0] = 5;
+  freqVector[1] = 2;
+  freqVector[2] = 8;
+  vectorSize = 3;
 }
 
 void loop() {
@@ -151,11 +258,11 @@ void loop() {
 
   // Check the buttons to determine the state
   // Priority increases the further you go
-  if (PLAY_RECORDING = digitalRead(playRecordingPin)) state = play_recording;
-  if (PLAY_LIVE = digitalRead(playLivePin)) state = play_live;
-  if (RECORD = digitalRead(recordPin)) state = record;
-  if (STOP = digitalRead(stopPin)) state = stop0;
-  if (RESET = digitalRead(resetPin)) {
+  if (PLAY_RECORDING = !digitalRead(playRecordingPin)) state = play_recording;
+  if (PLAY_LIVE = !digitalRead(playLivePin)) state = play_live;
+  if (RECORD = !digitalRead(recordPin)) state = record;
+  if (STOP = !digitalRead(stopPin)) state = stop0;
+  if (RESET = !digitalRead(resetPin)) {
     if (state != idle) {
       // The idle state is being entered.
       resetStartTime = millis();
@@ -163,7 +270,14 @@ void loop() {
     } else {
       resetCurrentTime = millis();
       if (resetCurrentTime - resetStartTime >= 3000) {
-        freqVector.clear();
+        lcd.setCursor(0, 1);
+        lcd.print("Dial:");
+        lcd.print(freqi);
+        lcd.print(", Stored: ");
+        for (int i = 0; i < totalVectorSize; i++) {
+          freqVector[i] = 0;
+        }
+        vectorSize = 0;
       }
     }
   }
@@ -176,7 +290,6 @@ void loop() {
       moveMotors(0);
       playFrequency(0);
       vectorPosition = 0;
-      currentFrequency = 0;
       storeButtonHeldDown = 0;
       //TODO update LCD
 
@@ -188,16 +301,27 @@ void loop() {
       //Update LCD
       playFrequency(0);
       storeButtonHeldDown = 0;
+      
       break;
 
 
      case record:
-      
+     noTone(speakerPin);
+      STORE = !digitalRead(storePin);
       //Update LCD
+      if (STORE) {
+        delay(50);
+        STORE = !digitalRead(storePin);
+      }
       if (STORE && !storeButtonHeldDown) {
-        storeButtonHeldDown = 1;
-        freqVector.push_back(freqi);
-      } else {
+        if(vectorSize < totalVectorSize) {
+          lcd.setCursor(15, 1);
+          lcd.print(freqi);
+          storeButtonHeldDown = 1;
+          freqVector[vectorSize] = freqi;
+          vectorSize++;
+        }
+      } else if (!STORE) {
         storeButtonHeldDown = 0;
       }
       break;
@@ -206,7 +330,7 @@ void loop() {
      case play_live:
       
       //Update LCD
-      if (STORE) {
+      if (!digitalRead(storePin)) {
         int newFreq = freqi;
         playFrequency(newFreq);
         moveMotors(newFreq);
@@ -215,24 +339,31 @@ void loop() {
 
 
      case play_recording:
+     lcd.setCursor(0, 1);
+     lcd.print(totalVectorSize);
+     lcd.print(" ");
+     lcd.print(vectorSize);
+     lcd.print(" ");
+     lcd.print(vectorPosition);
+     lcd.print(" ");
 
       //Update LCD
 
-      if(freqVector.size() == 0){
+      if(vectorSize == 0){
         playFrequency(0);
         break;
       }
       playingCurrentTime = millis(); // Read current time
+      if (vectorSize == vectorPosition) vectorPosition = 0;
       if (playingCurrentTime - playingStartTime >= 500 || !startedPlayingRecording) {
         startedPlayingRecording = 1;
         playingStartTime = millis();
-        if (freqVector.size() - 1 == vectorPosition) {
-          vectorPosition == 0;
-        } else {
-          vectorPosition++;
-        }
+
+        
         playFrequency(freqVector[vectorPosition]);
         moveMotors(freqVector[vectorPosition]);
+
+        vectorPosition++;
       }
       
       break;
